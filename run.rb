eb9458edbd36065c1b7cba5dac65f4b9d1981994
Viktor Dedek <38672169+vdedek@users.noreply.github.com>
2019-04-02 16:49:49 +0200

    pokus o předělání ans na array - nefunguje

---
+++ run.rb
@@ -0,0 +1,36 @@
+require 'timeout'
+
+# VERZE S METODOU
+
+def gets_with_timeout(sec, timeout_val = nil)
+  return gets.chomp if select([$stdin], nil, nil, sec)
+
+  timeout_val
+end
+
+# deset kancu
+kanec = 10
+ans = [nil]
+while kanec > 0
+  while kanec > 9
+    puts 'Běží na tebe kanec!'
+    ans [0] = gets_with_timeout(rand(5..10))
+    if ans.Include?('utíkám', 'utikam', 'bezim', 'běžím', 'běžím pryč')
+      puts 'Podařilo se ti utéci!'
+    else
+      puts 'Nabral tě!'
+      sleep rand(2..10)
+    end
+    kanec -= 1
+    sleep rand(2..10)
+  end
+  puts 'Běží na tebe další kanec!'
+  ans [0] = gets_with_timeout(rand(5..10))
+  if ans.Include?('utíkám', 'utikam', 'bezim', 'běžím', 'běžím pryč')
+    puts 'Utelk jsi!'
+  else
+    puts 'A nabral tě!'
+  end
+  sleep rand(2..10)
+  kanec -= 1
+end
